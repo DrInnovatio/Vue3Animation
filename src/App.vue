2333912ec27affb99216abae931345b05cf0791b
DrInnovatio <turing1723@gmail.com>
2022-02-19 23:39:30 +0900

    added zoom in-out animation.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-<button type="button" @click="flag = !flag">Toggle</button>
-<transition name="fade" mode="out-in"> 
-  <h2 v-if="flag" key="main">Hello Habok!!</h2>
-  <h2 v-else key="secondary">Rawon Ammiel Park</h2>
-</transition>
+  <button type="button" @click="flag = !flag">Toggle</button>
+  <!-- <transition name="fade" mode="out-in"> 
+    <h2 v-if="flag" key="main">Hello Habok!!</h2>
+    <h2 v-else key="secondary">Rawon Ammiel Park</h2>
+  </transition> -->
+
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello Habok!</h2>
+  </transition>
 </template>
 
 <script>
@@ -11,13 +15,19 @@ export default {
   name: "App",
   data(){
     return{
-      flag: false,
+      flag: true,
     }
   }
 };
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
 .fade-enter-from{
   opacity: 0;
 } 
@@ -29,5 +39,41 @@ export default {
 .fade-leave-to {
   transition: all 2s linear;
   opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
